0947d95053cc707276d14b49f25219daa310475c
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-04 20:24:33 -0400

    Update Main.java

--- src/TazwarBelal/Main.java
+++ src/TazwarBelal/Main.java
@@ -1,25 +1,34 @@
-package TazwarBelal;
+package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
+
+
         // write your code here
         System.out.println(Palindrome("racecar"));
+        System.out.println(cutOut("Alphamale"));
+
     }
-
-
-
-    public static boolean Palindrome(String str)
-    {
+    // write your code here
+    public static boolean Palindrome(String str) {
         String racecar = "";
-        for (int i = str.length(); i >1; i--) {
-            racecar = racecar + str.substring(i -1, i);
+        for (int i = str.length() + 1; i > 1; i--) {
+            racecar = racecar + str.substring(i - 2, i - 1);
         }
-        if (racecar.equals(str)){
+        if (racecar.equals(str)) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-}
+
+
+    public static String cutOut(String mainstr){
+              String substr = "cat";
+            return  mainstr.substring(2, mainstr.length()) + " " + substr ;
+
+
+        }
+
+    }
