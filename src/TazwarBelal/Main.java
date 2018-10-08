712946819a3b962e460a227e8389eeacc7b9b2dc
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-07 21:51:55 -0400

    Update Main.java

--- src/TazwarBelal/Main.java
+++ src/TazwarBelal/Main.java
@@ -8,7 +8,9 @@ public class Main {
         // write your code here
         System.out.println(Palindrome("racecar"));
         System.out.println(cutOut("Alphamale"));
-
+        System.out.println(sumUpTo(5));
+        System.out.println(primePrinter(3));
+        System.out.println(tensdigit(1267));
     }
     // write your code here
     public static boolean Palindrome(String str) {
@@ -31,4 +33,32 @@ public class Main {
 
         }
 
+    public static int sumUpTo(int num){
+        int sum = 0;
+        int i = 0;
+        while( i <= num){
+            sum = sum + i;
+            i++;
+        }
+        return sum;
     }
+
+        public static boolean primePrinter(int n) {
+            for (int i = 2; i <= n; i++) {
+                n =  n % i;
+            }
+            if (n != 0) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+    public static int tensdigit(int n){
+int output = n % 100;
+output = output/10;
+return output;
+    }
+
+            }
